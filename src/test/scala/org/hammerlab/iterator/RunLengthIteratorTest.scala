400f06dc8d79c0672b76b55e9ae785dde1934145
Ryan Williams <ryan.blake.williams@gmail.com>
2017-03-22 17:58:28 +0000

    add runLengthEncode api based on Ordering

--- src/test/scala/org/hammerlab/iterator/RunLengthIteratorTest.scala
+++ src/test/scala/org/hammerlab/iterator/RunLengthIteratorTest.scala
@@ -5,39 +5,69 @@ import org.hammerlab.test.Suite
 
 class RunLengthIteratorTest extends Suite {
 
-  def check[T](elems: T*)(expected: (T, Int)*): Unit = {
-    elems.iterator.runLengthEncode.toSeq should be(expected)
+  {
+    def check[T](elems: T*)(expected: (T, Int)*): Unit = {
+      elems.iterator.runLengthEncode().toSeq should be(expected)
+    }
+
+    test("empty") {
+      check()()
+    }
+
+    test("single") {
+      check("a")("a" → 1)
+    }
+
+    test("one run") {
+      check("a", "a", "a")("a" → 3)
+    }
+
+    test("two singletons") {
+      check("a", "b")("a" → 1, "b" → 1)
+    }
+
+    test("run singleton") {
+      check("a", "a", "a", "b")("a" → 3, "b" → 1)
+    }
+
+    test("singleton run") {
+      check("a", "b", "b", "b")("a" → 1, "b" → 3)
+    }
+
+    test("single run single") {
+      check("z", "y", "y", "x")("z" → 1, "y" → 2, "x" → 1)
+    }
+
+    test("two runs") {
+      check("a", "a", "a", "b", "b", "b", "b")("a" → 3, "b" → 4)
+    }
   }
 
-  test("empty") {
-    check()()
+  {
+    val parityOrdering =
+      Ordering.by[Int, Int](_ % 2)
+
+    def check(elems: Int*)(expected: (Int, Int)*): Unit = {
+      elems.iterator.runLengthEncode(parityOrdering).toSeq should be(expected)
+    }
+
+    test("evens and odds") {
+      check(
+        1, 5, 3, 7,
+        2,
+        5, 5,
+        4, 6, 8,
+        1,
+        2
+      )(
+        1 → 4,
+        2 → 1,
+        5 → 2,
+        4 → 3,
+        1 → 1,
+        2 → 1
+      )
+    }
   }
 
-  test("single") {
-    check("a")("a" -> 1)
-  }
-
-  test("one run") {
-    check("a", "a", "a")("a" -> 3)
-  }
-
-  test("two singletons") {
-    check("a", "b")("a" -> 1, "b" -> 1)
-  }
-
-  test("run singleton") {
-    check("a", "a", "a", "b")("a" -> 3, "b" -> 1)
-  }
-
-  test("singleton run") {
-    check("a", "b", "b", "b")("a" -> 1, "b" -> 3)
-  }
-
-  test("single run single") {
-    check("z", "y", "y", "x")("z" -> 1, "y" -> 2, "x" -> 1)
-  }
-
-  test("two runs") {
-    check("a", "a", "a", "b", "b", "b", "b")("a" -> 3, "b" -> 4)
-  }
 }
